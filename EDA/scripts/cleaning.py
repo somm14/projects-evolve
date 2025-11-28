d41214e64f7dfad76446dc2e3518d8cb3ffca4f4
somm14 <sorimalpica@gmail.com>
2025-11-28 15:40:48 +0100

    modificado

--- EDA/scripts/cleaning.py
+++ EDA/scripts/cleaning.py
@@ -1,32 +1,6 @@
 import pandas as pd
 import numpy as np
 
-def ordenar_cat(df, cols_cat, orden):
-    '''
-    Función para ordenar visualmente los valores de las variables categóricas ya que no tiene un orden con el que se pueda hacer sorted().
-
-    Args:
-    df (pd.DataFrame): DF desde el cual queremos recoger la información
-    cols_cat (list): lista de los nombres de las variables categóricas
-    orden (dict): diccionario del orden de los valores de cada variable.
-    frecuencia_relativa (bool): Si se quiere mostrar la frecuencia relativa. Por defecto, False.
-
-    Returns:
-    None: imprime el value_counts() de cada variable con sus valores ordenados de manera lógica.
-    '''
-    for col in cols_cat:
-        serie = df[col].value_counts()
-        if col in orden:
-            orden_ = orden[col]
-            categorias = serie.index.tolist()
-            categorias_ordenadas = [cat for cat in orden_ if cat in categorias]
-            if 'NULOS' in serie.index or None in serie.index:
-                categorias_ordenadas.append('NULOS')
-
-            serie = serie.reindex(categorias_ordenadas)
-            print(serie)
-            print('***' * 10)
-
 def clean_duplicated_thresh_nulls(df):
     '''
     Función que limpia el DF original de duplicados, manteniendo la primera ocurrencia, y eliminación de filas con más de 9 nulos entre sus valores.
